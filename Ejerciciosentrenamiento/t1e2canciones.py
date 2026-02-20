1df44b9a2384c7d628a54824df2f5f2cb01770a6
Antonionc10 <estmates1@gmail.com>
2026-02-20 13:49:07 +0100

    Update t1e2canciones.py

--- Ejerciciosentrenamiento/t1e2canciones.py
+++ Ejerciciosentrenamiento/t1e2canciones.py
@@ -1,2 +1,21 @@
 #Variables
-Cáncion="Her most beautiful smile"
+Canción="Her most beautiful smile"
+Artista="Taro Iwashiro"
+Album="Rurouni Kenshin: Ishin Shishi E No Requiem OST"
+Año="1997"
+Videoclip=False
+
+print("Nombre:", Canción)
+print("Artista:",Artista)
+print("Año:", Año)
+print("Videoclip:", Videoclip)
+
+
+nombre=input("¿Cuál es el nombre de la canción")
+print("dato 1:",Canción)
+año=int(input("¿De que año es"))
+print("dato 2:", Año)
+
+
+
+#¿¿Se pueden mezclar los input con las variables--> Los datos con variables???
