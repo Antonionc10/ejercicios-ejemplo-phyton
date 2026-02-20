4172b05fc42edd9f0e896a3835de2e8491ddcc8d
Antonionc10 <estmates1@gmail.com>
2026-02-20 13:34:05 +0100

    Update t1e1peliculas.py

--- Ejerciciosentrenamiento/t1e1peliculas.py
+++ Ejerciciosentrenamiento/t1e1peliculas.py
@@ -20,3 +20,22 @@ print("titulo",Título)
 Año=int(input("¿De que año es?"))
 print("Año", Año)
 
+#Peliculas
+Title = "The last Samurái"
+Year = "2003" 
+Director = "Edward Zwick"
+Gender = "Action/War"
+Duration = "2 Hours 34 minutes"
+Awards = True #Los booleanos no tienen que llevar las "" e ir en mayúsculas
+
+#Hay que tener cuidado, cuando lo quieres imprimir poner el nombre y la variable ¡¡NO el valor!!
+print( "Title:", Title)
+print( "Year:", Year)
+print( "Director:", Director)
+print( "Gender:", Gender)
+
+#Que pasa si yo quiero meter informacion al usuario.Si es una cadena(string)-->input   si es un numero-->int o float
+Title=input("¿Qué título tiene?")
+print("titulo",Title)
+Year=int(input("¿De que año es?"))
+print("Año", Year)
