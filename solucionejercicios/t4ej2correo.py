1d919bbac0464cc1cecaeafc4cd928f0b34d1406
Antonionc10 <estmates1@gmail.com>
2026-02-20 16:33:01 +0100

    Create t4ej2correo.py

---
+++ solucionejercicios/t4ej2correo.py
@@ -0,0 +1,10 @@
+correo=input("introduce tu correo:")
+
+longitud=len(correo)
+print("la longitud es:",longitud)
+
+mayusculas=correo.upper()
+print("Escrito en mayusculas es:",mayusculas)
+
+minusculas=correo.lower()
+print("Escrito en minuscula es:",minusculas)
